69478b8a71b5b2bf01770a2d361e3b318050e70e
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-08-18 15:58:26 +0700

    Move font imports to a dedicated file for better organization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/src/ui/css/globals.tailwind.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { geistMono, geistSans } from "@/src/ui/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
