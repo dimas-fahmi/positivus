2f1799b05c63ca83d4b4beed7282f93bac0c4f20
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-08-19 15:13:01 +0700

    Remove unnecessary padding from Services section for improved layout

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Services = () => {
   return (
-    <section id="services" className="py-16 px-4 md:px-0">
+    <section id="services" className="py-16 md:px-0">
       {/* Header */}
       <div className="flex flex-col md:flex-row gap-4">
         <Heading>Services</Heading>
