b76d9217446593dc07e02ca18307dfd51e6e003d
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-08-19 14:42:29 +0700

    Refactor Hero component for improved layout and responsiveness

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,15 +4,18 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section id="hero" className="grid font-mono grid-cols-2 min-h-[480px]">
+    <section
+      id="hero"
+      className="flex flex-col-reverse mb-16 md:mb-6 md:grid font-mono grid-cols-2 min-h-[480px]"
+    >
       {/* Hero Content */}
       <div className="flex items-center justify-center">
         {/* Content Wrapper */}
         <div>
-          <h1 className="font-bold text-6xl mb-4">
+          <h1 className="font-bold text-4xl text-center md:text-start mt-4 md:mt-0 md:text-6xl mb-4">
             Navigating the digital landscape for success
           </h1>
-          <p className="max-w-md mb-6">
+          <p className="max-w-md text-center md:text-start mb-6 block mx-auto md:mx-0">
             Our digital marketing agency helps businesses grow and succeed
             online through a range of services including SEO, PPC, social media
             marketing, and content creation.
@@ -21,14 +24,18 @@ const Hero = () => {
             Book a consultant
           </button> */}
 
-          <Button variant={"negative"} size={"xl"}>
+          <Button
+            variant={"negative"}
+            size={"xl"}
+            className="mx-auto md:mx-0 block"
+          >
             Book a consultant
           </Button>
         </div>
       </div>
 
       {/* Hero Illustration */}
-      <div className="flex items-center justify-end">
+      <div className="flex items-center justify-center md:justify-end">
         <Image
           width={500}
           height={500}
