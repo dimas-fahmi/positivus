5f03858c679732fdb948f42e417cf447a665cc6b
dimasfahmi <dimasfahmipebrian@gmail.com>
2025-08-19 16:31:18 +0700

    Add SectionHeader component for consistent section layout

---
+++ src/ui/components/SectionHeader.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Heading from "./Heading";
+import { cn } from "@/src/utils";
+
+export interface SectionHeaderProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  header: string;
+  subtitle: string;
+}
+
+const SectionHeader = React.forwardRef<HTMLDivElement, SectionHeaderProps>(
+  ({ className, header, subtitle, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className={cn(
+          "md:flex items-center gap-4 text-center md:text-start",
+          className
+        )}
+      >
+        <Heading className="mb-4 md:mb-0 mx-auto md:mx-0">{header}</Heading>
+        <p className="max-w-[60ch]">{subtitle}</p>
+      </div>
+    );
+  }
+);
+
+export default SectionHeader;
